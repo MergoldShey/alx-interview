23e9a1f66a8ff3861574e23c15dc8b9c94cb0aec
MergoldShey <mergoldshey@gmail.com>
2023-04-21 04:18:07 +0000

    Interview

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -17,8 +17,11 @@ def pascal_trinangle(n):
 
     for i in range (1, n):
         row = [1]
+
         for j in range (1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
         row.append(1)
         triangle.append(row)
-     return triangle   
+
+    return triangle   
