6f9bec81f89b1814093f18d55e37b6ddfb9f8d19
MergoldShey <mergoldshey@gmail.com>
2023-06-26 02:37:35 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -13,5 +13,4 @@ def island_perimeter(grid):
     for row in grid + list(map(list, zip(*grid))):
         for i1, i2 in zip([0] + row, row + [0]):
             area += int(i1 != i2)
-    return area 
-
+    return area
